2eb9249da5dac84e0b10d057022418c90aa6614e
hptg1994 <hptg1994@gmail.com>
2018-09-07 20:36:49 -0400

    Done

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const electron = require("electron");
 
 const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require("fluent-ffmpeg")
+
+let mainWindow;
 
 app.on('ready', () => {
-  let mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 });
+
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (error, metadata) => {
+    mainWindow.webContents.send("video:metadata".metadata.format.duration);
+  })
+})
